f13ef851e763359ecdcecf8dc222087fd5d12421
Hridoy <19202103103@cse.bubt.edu.bd>
2023-11-19 12:50:15 +0600

    search and sort function add

--- learnathon_hridoy/src/components/home/Home.tsx
+++ learnathon_hridoy/src/components/home/Home.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import DeleteData from "../deleteData/DeleteData.tsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSort } from '@fortawesome/free-solid-svg-icons'
 interface User {
   id: number;
   name: string;
@@ -9,72 +11,150 @@ interface User {
   username: string;
   phone: string;
   website: string;
-  i: number;
 }
+const headers = [
+  { key: 'id', label: 'ID' },
+  { key: 'name', label: 'Name' },
+  { key: 'username', label: 'User name' },
+  { key: 'email', label: 'Email' },
+  { key: 'phone', label: 'Phone no.' },
+  { key: 'website', label: 'Website' }
+];
+
+
+
+
+
 function Home() {
+  const [userData, setUserData] = useState([]);
+  const [userValue, setUserValue] = useState("");
+  const [ sortValue, setSortValue] = useState("");
 
-  const [userData, setUserData] = useState([])
 
-  useEffect(()=>{
+  const sortBy = ["id", "name", "email", "username", "phone", "website" ];
+
+  useEffect(() => {
     loadUsersData();
-  },[]
-  )
-  const loadUsersData =async () => {
+  }, []);
+  const loadUsersData = async () => {
     return await axios
-    .get('http://localhost:3000/users')
-    .then(getData => setUserData(getData.data))
-    .catch(err => console.log(err));
-  }  
-  const handleDelete = (id: number) =>{
-    const confirm = window.confirm("Are you sure to Delete?");
-    if(confirm)
-    {
-      axios.delete('http://localhost:3000/users/'+id)
-      .then( () =>{
-        location.reload(); // this will refresh the tab
-      })
-      .catch(error => console.log(error));
-    }
+      .get("http://localhost:3000/users")
+      .then((getData) => setUserData(getData.data))
+      .catch((err) => console.log(err));
+  };
+
+
+  const handleSort = async (e: { target: { value: string; }; }) => {
+    const sortingValue = e.target.value;
+    setSortValue(sortingValue)
+    return await axios
+    .get(`http://localhost:3000/users?_sort=${sortingValue}&_order=asc`)
+    .then((getData) => setUserData(getData.data))
+    .catch((err) => console.log(err));
   }
 
+  const handleReset = () =>{
+    loadUsersData();
+  }
+  const handleSearch = async (e: { preventDefault: () => void; }) =>{
+    e.preventDefault();
+    return await axios
+      .get(`http://localhost:3000/users?q=${userValue}`)
+      .then((getData) => {setUserData(getData.data);
+      setUserValue("");
+      })
+      .catch((err) => console.log(err));
+  }
+
+
   return (
-    <div className="d-flex bg-light text-dark flex-column justify-content-center align-items-center vh-100">
+    <div className = "d-flex bg-light text-dark flex-column justify-content-center align-items-center vh-100">
       <h1>list of users</h1>
-      <div className="w-75 rounded bg-white border shadow p-4">
+      <div className = "w-75 rounded bg-white border shadow p-4">
+        <div className = "d-flex justify-content-start">
+          <form className = "d-flex input-group w-auto" onSubmit={handleSearch}>
+            <input type ="text" 
+            className ="form-control"
+            placeholder ="Search name"
+            value = {userValue}
+            onChange = {(e) => setUserValue(e.target.value)}
+            />
+            <button type="submit">Search</button>
+            <button onClick={() => handleReset()}>Reset</button>
+          </form>
+        </div>
         <div className="d-flex justify-content-end">
-          <Link to="/create" className="btn btn-primary mb-3">Add Member</Link></div>
+          <Link to="/create" className="btn btn-primary mb-3">
+            Add Member
+          </Link>
+        </div>
         <table className="table">
           <thead className="table-dark text-center">
             <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>UserName</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Website</th>
+              {headers.map((row) => {
+                return <td key={row.key}>{row.label}</td>
+              })
+
+              }
+              {/* <th>ID <FontAwesomeIcon icon={faSort}/></th>
+              <th>Name <button value={"name"} onClick={()=>handleSort("name")}><FontAwesomeIcon icon={faSort}/></button></th>
+              <th>UserName <FontAwesomeIcon icon={faSort}/></th>
+              <th>Email <FontAwesomeIcon icon={faSort}/></th>
+              <th>Phone <FontAwesomeIcon icon={faSort}/></th>
+              <th>Website <FontAwesomeIcon icon={faSort}/></th> */}
               <th>Action</th>
             </tr>
           </thead>
-          <tbody className="text-center">
-            {
-              userData.map((data:User , i) => (
-                <tr key={ i }>
-                  <td>{ data.id }</td>
-                  <td>{ data.name }</td>
-                  <td>{ data.username }</td>
-                  <td>{ data.email }</td>
-                  <td>{ data.phone }</td>
-                  <td>{ data.website }</td>
+            {userData.length === 0 ? (
+              <tbody className="text-center">
+              <tr>
+                <td colSpan={8}>NO DATA FOUND</td>
+              </tr>
+               </tbody>
+            ) : (
+              userData.map((data: User, i) => (
+                <tbody className="text-center">
+                <tr key={i}>
+                  <td>{data.id}</td>
+                  <td>{data.name}</td>
+                  <td>{data.username}</td>
+                  <td>{data.email}</td>
+                  <td>{data.phone}</td>
+                  <td>{data.website}</td>
                   <td>
-                    <Link to={`/read/${data.id}`} className="btn btn-outline-success me-3">Read</Link>
+                    <Link
+                      to={`/read/${data.id}`}
+                      className="btn btn-outline-success me-3"
+                    >
+                      Read
+                    </Link>
                     {/* <Link to={`/update/${data.id}`}  className="btn btn-outline-success me-3">Update</Link> */}
-                    <button onClick={ () => handleDelete(data.id) } className="btn btn-outline-danger">Delete</button>
+                    <button
+                      onClick={() => DeleteData(data.id)}
+                      className="btn btn-outline-danger"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
+             </tbody>
               ))
-            }
-          </tbody>
+            )}
         </table>
+        <h6>Sort Data By: </h6>
+        <select style={{ width:"200px", height: "30px" }}
+        onChange={handleSort}
+        value={sortValue}
+        >
+          <option>Select value</option>
+          {
+            sortBy.map((item, index) => (
+              <option value={item} key={index}>
+                {item}
+              </option>
+            ))
+          }
+        </select>
       </div>
     </div>
   );
