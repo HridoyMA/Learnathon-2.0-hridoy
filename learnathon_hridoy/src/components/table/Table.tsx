be36a28b4d40186564ce7daeb58ae26571798aea
Hridoy <19202103103@cse.bubt.edu.bd>
2023-11-20 00:27:51 +0600

    css update

--- learnathon_hridoy/src/components/table/Table.tsx
+++ learnathon_hridoy/src/components/table/Table.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DeleteData from "../deleteData/DeleteData.tsx";
-import './table.css'
+import "./table.css";
 // import { MDBDatatable } from 'mdb-react-ui-kit';
 
 interface User {
@@ -62,13 +62,17 @@ function Table() {
 
   return (
     <div className="d-flex bg-light text-dark flex-column justify-content-center align-items-center vh-100">
-      {/* <h1>List Of Players</h1> */}
       <div className="ft-1">
-                            <h3><span>LIST </span>OF PLAYERS</h3>
-                        </div>
+        <h3>
+          <span>LIST </span>OF PLAYERS
+        </h3>
+      </div>
       <div className="w-75 rounded bg-white border shadow p-4">
         <div className="d-flex">
-          <form className="d-flex input-group z-0 w-auto" onSubmit={handleSearch}>
+          <form
+            className="d-flex input-group z-0 w-auto"
+            onSubmit={handleSearch}
+          >
             <input
               type="text"
               className="form-control"
@@ -76,66 +80,75 @@ function Table() {
               value={userValue}
               onChange={(e) => setUserValue(e.target.value)}
             />
-            <button type="submit" className="btn btn-outline-info">Search</button>
-            <button onClick={() => handleReset()} className="btn btn-outline-dark">Reset</button>
+            <button type="submit" className="btn btn-outline-info">
+              Search
+            </button>
+            <button
+              onClick={() => handleReset()}
+              className="btn btn-outline-dark"
+            >
+              Reset
+            </button>
           </form>
         </div>
         <div className="d-flex justify-content-end">
           <Link to="/create" className="btn btn-primary mb-3 me-3">
             Add Member
           </Link>
-          <Link to="/" className="btn btn-primary mb-3">Back</Link>
+          <Link to="/" className="btn btn-primary mb-3">
+            Back
+          </Link>
         </div>
         <div className="table-wrapper">
-        <table className="table">
-          <thead className="table-dark text-center table-fixed">
-            <tr>
-              {headers.map((row) => {
-                return <td key={row.key}>{row.label}</td>;
-              })}
-              <th>Action</th>
-            </tr>
-          </thead>
-          {userData.length === 0 ? (
-            <tbody className="text-center">
+          <table className="table">
+            <thead className="table-dark text-center table-fixed">
               <tr>
-                <td colSpan={8}>NO DATA FOUND</td>
+                {headers.map((row) => {
+                  return <td key={row.key}>{row.label}</td>;
+                })}
+                <th>Action</th>
               </tr>
-            </tbody>
-          ) : (
-            userData.map((data: User, i) => (
+            </thead>
+            {userData.length === 0 ? (
               <tbody className="text-center">
-                <tr key={i}>
-                  <td>{data.id}</td>
-                  <td>{data.name}</td>
-                  <td>{data.username}</td>
-                  <td>{data.email}</td>
-                  <td>{data.phone}</td>
-                  <td>{data.website}</td>
-                  <td>
-                    <Link
-                      to={`/read/${data.id}`}
-                      className="btn btn-outline-success me-3"
-                    >
-                      Read
-                    </Link>
-                    {/* <Link to={`/update/${data.id}`}  className="btn btn-outline-success me-3">Update</Link> */}
-                    <button
-                      onClick={() => DeleteData(data.id)}
-                      className="btn btn-outline-danger"
-                    >
-                      Delete
-                    </button>
-                  </td>
+                <tr>
+                  <td colSpan={8}>NO DATA FOUND</td>
                 </tr>
               </tbody>
-            ))
-          )}
-        </table>
+            ) : (
+              userData.map((data: User, i) => (
+                <tbody className="text-center">
+                  <tr key={i}>
+                    <td>{data.id}</td>
+                    <td>{data.name}</td>
+                    <td>{data.username}</td>
+                    <td>{data.email}</td>
+                    <td>{data.phone}</td>
+                    <td>{data.website}</td>
+                    <td>
+                      <Link
+                        to={`/read/${data.id}`}
+                        className="btn btn-outline-success me-3"
+                      >
+                        Read
+                      </Link>
+                      {/* <Link to={`/update/${data.id}`}  className="btn btn-outline-success me-3">Update</Link> */}
+                      <button
+                        onClick={() => DeleteData(data.id)}
+                        className="btn btn-outline-danger"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              ))
+            )}
+          </table>
         </div>
         <h6>Sort Data By: </h6>
         <select
-          style={{ width: "200px", height: "30px", borderRadius:"5px" }}
+          style={{ width: "200px", height: "30px", borderRadius: "5px" }}
           onChange={handleSort}
           value={sortValue}
         >
